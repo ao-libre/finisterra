4c3837f0537759b73c1426ece1f1c88c824a4c76
Guido Joaquin Tamborindeguy <guido@tamborindeguy.com.ar>
2019-05-09 23:32:56 -0300

    Adding function to scale graphics to 2x (awesome?)

---
+++ design/src/images/ScaleImagesKt.kt
@@ -0,0 +1,29 @@
+package images
+
+import com.soywiz.kaifu2x.Kaifu2x
+import com.soywiz.korim.bitmap.BitmapChannel
+import com.soywiz.korim.format.*
+import com.soywiz.korio.Korio
+import com.soywiz.korio.vfs.UniversalVfs
+
+object Scale2x {
+    @JvmStatic
+    fun run(input : String, output : String) = Korio {
+        defaultImageFormats.registerStandard()
+        val image = UniversalVfs(input).readBitmapNoNative().toBMP32()
+
+        val noiseReduction = 0
+
+        val channels = BitmapChannel.ALL.toList()
+        val noiseReductedImage = Kaifu2x.noiseReductionRgba(image, noiseReduction, channels, true, chunkSize = 128)
+        val scaledImage = Kaifu2x.scaleRgba(image, 2, channels, true, 128)
+
+        val outFile = UniversalVfs(output).ensureParents()
+        scaledImage.writeTo(outFile, ImageEncodingProps(quality = 100.toDouble() / 100.0))
+    }
+
+
+
+}
+
+
--- design/src/launcher/DesignCenter.java
+++ design/src/launcher/DesignCenter.java
@@ -1,7 +1,11 @@
 package launcher;
 
 import com.badlogic.gdx.Game;
-import json.GraphicsToJson;
+import images.Scale2x;
+
+import java.io.File;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class DesignCenter extends Game {
 
@@ -12,6 +16,24 @@ public class DesignCenter extends Game {
     public void create() {
 //        SeparateObjByType.run(OUTPUT_FOLDER);
 //        SpellsToJson.run(OUTPUT_FOLDER);
-        GraphicsToJson.run(OUTPUT_FOLDER);
+//        GraphicsToJson.run(OUTPUT_FOLDER);
+
+        File graficosInput = new File("data/graficos/");
+        File graficosOutput = new File("data/graficos2x/");
+        if (!graficosOutput.exists()) {
+            graficosOutput.mkdir();
+        }
+
+        Stream.of(Objects.requireNonNull(graficosInput.list())).forEach(fileName -> {
+            if (fileName.endsWith(".png")) {
+                String inputFile = graficosInput.getPath() + "/" + fileName;
+                String outputFile = graficosOutput.getPath() + "/" + fileName;
+                if (!new File(graficosOutput + "/" + fileName).exists()) {
+                    Scale2x.run(inputFile, outputFile);
+                }
+            }
+        });
+
+
     }
 }
