d8bf57a353f75a9568fa5c5f6ef9fd07190e7048
Derillo <donderillo@gmail.com>
2019-05-14 23:53:03 -0300

    Add KeyConfigAlternative
    Otra configuración alternativa para el teclado

--- client/src/game/utils/AlternativeKeys.java
+++ client/src/game/utils/AlternativeKeys.java
@@ -23,3 +23,24 @@ public class AlternativeKeys {
 
 
 }
+public class AlternativeKeysTwo {
+
+    public static final int ATTACK_1 = Input.Keys.SHIFT_LEFT;
+    public static final int ATTACK_2 = Input.Keys.Space;
+    public static final int MEDITATE = Input.Keys.X;
+    public static final int USE = Input.Keys.F;
+    public static final int HIDE = Input.Keys.C;
+    public static final int INVENTORY = Input.Keys.I;
+    public static final int SPELLS = Input.Keys.K;
+    public static final int TALK = Input.Keys.ENTER;
+    public static final int DROP = Input.Keys.T;
+    public static final int TAKE = Input.Keys.L;
+    public static final int EQUIP = Input.Keys.E;
+
+    public static int MOVE_LEFT = Input.Keys.A;
+    public static int MOVE_RIGHT = Input.Keys.D;
+    public static int MOVE_UP = Input.Keys.W;
+    public static int MOVE_DOWN = Input.Keys.S;
+
+
+}
