064727e13a99b64095c67154c9705172179887f5
Jopi <jopiodz00@gmail.com>
2019-05-26 00:26:03 -0300

    Upgraded lwjgl to lwjgl3 and partially fixed design function.

--- design/src/launcher/DesignCenterLauncher.java
+++ design/src/launcher/DesignCenterLauncher.java
@@ -1,19 +1,23 @@
 package launcher;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class DesignCenterLauncher {
     public static void main(String[] arg) {
         System.setProperty("org.lwjgl.opengl.Display.enableOSXFullscreenModeAPI", "true");
-        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "Finisterra Design Center";
-        cfg.width = 800;
-        cfg.height = 600;
-        cfg.fullscreen = false;
-        cfg.vSyncEnabled = true;
-        cfg.foregroundFPS = 0;
-        cfg.resizable = true;
-        new LwjglApplication(new DesignCenter(), cfg);
+        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+
+        Graphics.DisplayMode mode = Gdx.graphics.getDisplayMode();
+
+        cfg.setTitle("Finisterra Design Center");
+        cfg.setWindowedMode(800,600);
+        cfg.setFullscreenMode(mode);
+        cfg.useVsync(true);
+        cfg.setIdleFPS(0);
+        cfg.setResizable(true);
+        new Lwjgl3Application(new DesignCenter(), cfg);
     }
 }
