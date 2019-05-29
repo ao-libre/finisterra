f8836f0851b7c175f1f1a5c163952a0c63b550c9
Guido Joaquin Tamborindeguy <guido@tamborindeguy.com.ar>
2019-05-28 21:34:43 -0300

    Adding missing '.build()'

--- server/src/server/systems/BuffSystem.java
+++ server/src/server/systems/BuffSystem.java
@@ -5,13 +5,12 @@ import com.artemis.E;
 import com.artemis.FluidIteratingSystem;
 import entity.character.states.Buff;
 import server.systems.manager.WorldManager;
-import shared.network.notifications.EntityUpdate;
 import shared.network.notifications.EntityUpdate.EntityUpdateBuilder;
 
 public class BuffSystem extends FluidIteratingSystem {
 
     public BuffSystem() {
-        super(Aspect.all(Buff.class));;
+        super(Aspect.all(Buff.class));
     }
 
     @Override
@@ -35,9 +34,8 @@ public class BuffSystem extends FluidIteratingSystem {
                 } else {
                     update.withComponents(buff);
                 }
-                world.getSystem(WorldManager.class).sendEntityUpdate(e.id(), update);
+                world.getSystem(WorldManager.class).sendEntityUpdate(e.id(), update.build());
             }
-
         });
 
     }
