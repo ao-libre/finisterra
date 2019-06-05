31c13c1502ee6aa167afb5da7114bb7af6cae390
Recox <5874806+RecoX@users.noreply.github.com>
2019-06-05 23:31:23 +1200

    Another attempt to make the gson library work

--- shared/src/shared/util/Messages.java
+++ shared/src/shared/util/Messages.java
@@ -1,68 +1,137 @@
 package shared.util;
 
 import java.io.FileReader;
+import java.io.FileNotFoundException;
 import com.google.gson.Gson;
 //import game.utils.Resources;
 
 public class Messages {
 
+    private class JsonMessages {
+        public String DEAD_CANT_ATTACK;
+        public String CANT_ATTACK_DEAD;
+        public String CANT_ATTACK_CITIZEN;
+        public String NOT_ENOUGH_ENERGY;
+        public String ATTACK_FAILED;
+        public String ATTACKED_AND_FAILED;
+        public String SHIELD_DEFENSE;
+        public String DEFENDED_WITH_SHIELD;
+        public String KILL;
+        public String KILLED;
+        public String USER_CRITIC_HIT;
+        public String VICTIM_CRITIC_HIT;
+        public String USER_STAB_HIT;
+        public String VICTIM_STAB_HIT;
+        public String USER_NORMAL_HIT;
+        public String VICTIM_NORMAL_HIT;
+        public String DAMAGE_TO;
+        public String DAMAGED_BY;
+        public String HEAL_TO;
+        public String HEAL_BY;
+        public String INVALID_TARGET;
+        public String NOT_ENOUGHT_MANA;
+        public String NOT_PARALYSIS;
+        public String CANT_ATTACK_YOURSELF;
+        public String MANA_RECOVERED;
+        public String MEDITATE_STOP;
+        public String MEDITATE_START;
+        public String MANA_FULL;
+        public String SEE_NOTHING_INTEREST;
+        public String SEE_SOMEONE;
+    }
+
     private void init() {
-        Gson gson = new Gson();
+        try
+        {
+            Gson gson = new Gson();
+            JsonMessages JsonLanguageObject = gson.fromJson(new FileReader("C:\\resources\\languages\\english.json"), JsonMessages.class);
 
-        // 1. JSON file to Java object
-        Object JsonLanguage = gson.fromJson(new FileReader("C:\\resources\\english.json"), Object.class);
-
-        System.out.println(JsonLanguage);
+            this.DEAD_CANT_ATTACK = JsonLanguageObject.DEAD_CANT_ATTACK;
+            this.CANT_ATTACK_DEAD = JsonLanguageObject.CANT_ATTACK_DEAD;
+            this.CANT_ATTACK_CITIZEN = JsonLanguageObject.CANT_ATTACK_CITIZEN;
+            this.NOT_ENOUGH_ENERGY = JsonLanguageObject.NOT_ENOUGH_ENERGY;
+            this.ATTACK_FAILED = JsonLanguageObject.ATTACK_FAILED;
+            this.ATTACKED_AND_FAILED = JsonLanguageObject.ATTACKED_AND_FAILED;
+            this.SHIELD_DEFENSE = JsonLanguageObject.SHIELD_DEFENSE;
+            this.DEFENDED_WITH_SHIELD = JsonLanguageObject.DEFENDED_WITH_SHIELD;
+            this.KILL = JsonLanguageObject.KILL;
+            this.KILLED = JsonLanguageObject.KILLED;
+            this.USER_CRITIC_HIT = JsonLanguageObject.USER_CRITIC_HIT;
+            this.VICTIM_CRITIC_HIT = JsonLanguageObject.VICTIM_CRITIC_HIT;
+            this.USER_STAB_HIT = JsonLanguageObject.USER_STAB_HIT;
+            this.VICTIM_STAB_HIT = JsonLanguageObject.VICTIM_STAB_HIT;
+            this.USER_NORMAL_HIT = JsonLanguageObject.USER_NORMAL_HIT;
+            this.VICTIM_NORMAL_HIT = JsonLanguageObject.VICTIM_NORMAL_HIT;
+            this.DAMAGE_TO = JsonLanguageObject.DAMAGE_TO;
+            this.DAMAGED_BY = JsonLanguageObject.DAMAGED_BY;
+            this.HEAL_TO = JsonLanguageObject.HEAL_TO;
+            this.HEAL_BY = JsonLanguageObject.HEAL_BY;
+            this.INVALID_TARGET = JsonLanguageObject.INVALID_TARGET;
+            this.NOT_ENOUGHT_MANA = JsonLanguageObject.NOT_ENOUGHT_MANA;
+            this.NOT_PARALYSIS = JsonLanguageObject.NOT_PARALYSIS;
+            this.CANT_ATTACK_YOURSELF = JsonLanguageObject.CANT_ATTACK_YOURSELF;
+            this.MANA_RECOVERED = JsonLanguageObject.MANA_RECOVERED;
+            this.MEDITATE_STOP = JsonLanguageObject.MEDITATE_STOP;
+            this.MEDITATE_START = JsonLanguageObject.MEDITATE_START;
+            this.MANA_FULL = JsonLanguageObject.MANA_FULL;
+            this.SEE_NOTHING_INTEREST = JsonLanguageObject.SEE_NOTHING_INTEREST;
+            this.SEE_SOMEONE = JsonLanguageObject.SEE_SOMEONE;
+        }
+        catch (FileNotFoundException ex)
+        {
+            System.out.println("ERROR: No language file found");
+            System.exit(0);
+        }
 
     }
 
     // PHYSICAL COMBAT
-    public final static String DEAD_CANT_ATTACK = JsonLanguage.get("DEAD_CANT_ATTACK");
-    public final static String CANT_ATTACK_DEAD = JsonLanguage.get("CANT_ATTACK_DEAD");
+    public static String DEAD_CANT_ATTACK;
+    public static String CANT_ATTACK_DEAD;
 
-    public final static String CANT_ATTACK_CITIZEN = JsonLanguage.get("CANT_ATTACK_CITIZEN");
-    public final static String NOT_ENOUGH_ENERGY = JsonLanguage.get("NOT_ENOUGH_ENERGY");
+    public static String CANT_ATTACK_CITIZEN;
+    public static String NOT_ENOUGH_ENERGY;
 
-    public final static String ATTACK_FAILED = JsonLanguage.get("ATTACK_FAILED");
-    public final static String ATTACKED_AND_FAILED = JsonLanguage.get("ATTACKED_AND_FAILED");
+    public static String ATTACK_FAILED;
+    public static String ATTACKED_AND_FAILED;
 
-    public final static String SHIELD_DEFENSE = JsonLanguage.get("SHIELD_DEFENSE");
-    public final static String DEFENDED_WITH_SHIELD = JsonLanguage.get("DEFENDED_WITH_SHIELD");
+    public static String SHIELD_DEFENSE;
+    public static String DEFENDED_WITH_SHIELD;
 
-    public final static String KILL = JsonLanguage.get("KILL");
-    public final static String KILLED = JsonLanguage.get("KILLED");
+    public static String KILL;
+    public static String KILLED;
 
-    public static final String USER_CRITIC_HIT = JsonLanguage.get("USER_CRITIC_HIT");
-    public static final String VICTIM_CRITIC_HIT = JsonLanguage.get("VICTIM_CRITIC_HIT");
+    public static String USER_CRITIC_HIT;
+    public static String VICTIM_CRITIC_HIT;
 
-    public static final String USER_STAB_HIT = JsonLanguage.get("USER_STAB_HIT");
-    public static final String VICTIM_STAB_HIT = JsonLanguage.get("VICTIM_STAB_HIT");
+    public static String USER_STAB_HIT;
+    public static String VICTIM_STAB_HIT;
 
-    public static final String USER_NORMAL_HIT = JsonLanguage.get("USER_NORMAL_HIT");
-    public static final String VICTIM_NORMAL_HIT = JsonLanguage.get("VICTIM_NORMAL_HIT");
+    public static String USER_NORMAL_HIT;
+    public static String VICTIM_NORMAL_HIT;
 
     // MAGIC COMBAT
-    public static final String DAMAGE_TO = JsonLanguage.get("DAMAGE_TO");
-    public static final String DAMAGED_BY = JsonLanguage.get("DAMAGED_BY");
+    public static String DAMAGE_TO;
+    public static String DAMAGED_BY;
 
-    public final static String HEAL_TO = JsonLanguage.get("HEAL_TO");
-    public final static String HEAL_BY = JsonLanguage.get("HEAL_BY");
+    public static String HEAL_TO;
+    public static String HEAL_BY;
 
-    public static final String INVALID_TARGET = JsonLanguage.get("INVALID_TARGET");
-    public static final String NOT_ENOUGHT_MANA = JsonLanguage.get("NOT_ENOUGHT_MANA");
+    public static String INVALID_TARGET;
+    public static String NOT_ENOUGHT_MANA;
 
-    public static final String NOT_PARALYSIS = JsonLanguage.get("NOT_PARALYSIS");
-    public static final String CANT_ATTACK_YOURSELF = JsonLanguage.get("CANT_ATTACK_YOURSELF");
+    public static String NOT_PARALYSIS;
+    public static String CANT_ATTACK_YOURSELF;
 
     // MANA
-    public static final String MANA_RECOVERED = JsonLanguage.get("MANA_RECOVERED");
-    public static final String MEDITATE_STOP = JsonLanguage.get("MEDITATE_STOP");
-    public static final String MEDITATE_START = JsonLanguage.get("MEDITATE_START");
-    public static final String MANA_FULL = JsonLanguage.get("MANA_FULL");
+    public static String MANA_RECOVERED;
+    public static String MEDITATE_STOP;
+    public static String MEDITATE_START;
+    public static String MANA_FULL;
 
 
     // OTHERS
-    public static final String SEE_NOTHING_INTEREST = JsonLanguage.get("SEE_NOTHING_INTEREST");
-    public static final String SEE_SOMEONE = JsonLanguage.get("SEE_SOMEONE");
+    public static String SEE_NOTHING_INTEREST;
+    public static String SEE_SOMEONE;
 
 }
