e1f7bd9eac3df3a39e56622d241bca7925511ec1
Joaquín <jopiodz00@gmail.com>
2019-06-06 19:46:10 -0300

    Implemented LWJGL3-backend.

--- desktop/src/launcher/DesktopLauncher.java
+++ desktop/src/launcher/DesktopLauncher.java
@@ -1,28 +1,21 @@
 package launcher;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import game.AOGame;
 
 public class DesktopLauncher {
 
-    // TODO: Read from config file?
-    public static final int GAME_SCREEN_WIDTH = 1280;
-    public static final int GAME_SCREEN_HEIGHT = 768;
-    public static final boolean GAME_FULL_SCREEN = false;
-    public static final boolean GAME_VSYNC_ENABLED = true;
-
     public static void main(String[] arg) {
         System.setProperty("org.lwjgl.opengl.Display.enableOSXFullscreenModeAPI", "true");
-        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-        cfg.title = "Finisterra";
-        cfg.width = GAME_SCREEN_WIDTH;
-        cfg.height = GAME_SCREEN_HEIGHT;
-        cfg.fullscreen = GAME_FULL_SCREEN;
-        cfg.vSyncEnabled = GAME_VSYNC_ENABLED;
-        cfg.foregroundFPS = 0;
-        cfg.resizable = true;
-        cfg.forceExit = false;
-        new LwjglApplication(new AOGame(), cfg);
+
+        Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
+            cfg.setTitle("Finisterra");
+            cfg.setWindowedMode(1280,720);
+            cfg.useVsync(true);
+            cfg.setIdleFPS(60);
+            cfg.setResizable(true);
+
+        new Lwjgl3Application(new AOGame(), cfg);
     }
 }
