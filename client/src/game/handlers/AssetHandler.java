59b6e33805e77878601b140accfda4672240c921
Joaquín <jopiodz00@gmail.com>
2019-06-06 15:39:55 -0300

    Implemented `dispose()` to properly close the application.

--- client/src/game/handlers/AssetHandler.java
+++ client/src/game/handlers/AssetHandler.java
@@ -1,6 +1,7 @@
 package game.handlers;
 
 import com.badlogic.gdx.Gdx;
+import org.lwjgl.openal.AL;
 
 public class AssetHandler {
 
@@ -31,7 +32,9 @@ public class AssetHandler {
     }
 
     public static void unload() {
-        // TODO: Implement this
+        MusicHandler.unload();
+        SoundsHandler.unload();
+        AL.destroy();
     }
 
     public static StateHandler getState() {
