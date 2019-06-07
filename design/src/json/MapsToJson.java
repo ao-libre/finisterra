e30f132fb967d35d689356efd164a2f9d498d22c
Guido Joaquin Tamborindeguy <guido@tamborindeguy.com.ar>
2019-06-06 22:33:33 -0300

    Converting maps into json and adding neighbours data

---
+++ design/src/json/MapsToJson.java
@@ -0,0 +1,36 @@
+package json;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.Json;
+import shared.model.map.Map;
+import shared.util.AOJson;
+import shared.util.MapHelper;
+
+import java.util.HashMap;
+
+public class MapsToJson {
+
+    public static void transformToJson() {
+        MapHelper helper = MapHelper.instance();
+        helper.getMaps().forEach((id, map) -> {
+            map.setNeighbours(
+                    helper.getMap(MapHelper.Dir.LEFT, map),
+                    helper.getMap(MapHelper.Dir.UP, map),
+                    helper.getMap(MapHelper.Dir.RIGHT, map),
+                    helper.getMap(MapHelper.Dir.DOWN, map)
+            );
+        });
+        saveMaps(helper.getMaps());
+    }
+
+    private static void saveMaps(HashMap<Integer, Map> maps) {
+        Json json = new AOJson();
+        FileHandle folder = Gdx.files.local("output/maps/");
+        maps.forEach((id, map) -> {
+            FileHandle mapFile = folder.child("Map" + id + ".json");
+            json.toJson(map, mapFile);
+        });
+    }
+
+}
