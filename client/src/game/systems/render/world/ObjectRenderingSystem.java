5b9bb0ac62d77b4ad501b7957d8840e931209d5f
Guido Joaquin Tamborindeguy <guido@tamborindeguy.com.ar>
2019-06-18 01:23:49 -0300

    Popup animation on object drop

--- client/src/game/systems/render/world/ObjectRenderingSystem.java
+++ client/src/game/systems/render/world/ObjectRenderingSystem.java
@@ -4,10 +4,13 @@ import com.artemis.Aspect;
 import com.artemis.E;
 import com.artemis.annotations.Wire;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Interpolation;
 import entity.world.Object;
 import game.handlers.ObjectHandler;
 import position.Pos2D;
 import position.WorldPos;
+import shared.model.map.Tile;
 import shared.objects.types.Obj;
 import shared.util.Util;
 
@@ -28,7 +31,19 @@ public class ObjectRenderingSystem extends RenderingSystem {
         object.ifPresent(obj -> {
             WorldPos objectPos = e.getWorldPos();
             Pos2D screenPos = Util.toScreen(objectPos);
-            getBatch().draw(ObjectHandler.getIngameGraphic(obj), screenPos.x, screenPos.y);
+            // TODO popup animation
+            if (!e.hasScale()) {
+                e.scale(0f);
+            } else if(e.getScale().scale >= 1.0f) {
+                e.getScale().scale = 1f;
+            } else {
+                e.getScale().scale += world.delta * 2;
+            }
+            float scale = Interpolation.swingOut.apply(e.getScale().scale);
+            TextureRegion texture = ObjectHandler.getIngameGraphic(obj);
+            float width = scale * texture.getRegionWidth();
+            float height = scale * texture.getRegionHeight();
+            getBatch().draw(texture, screenPos.x + (Tile.TILE_PIXEL_WIDTH - width) / 2, screenPos.y + (Tile.TILE_PIXEL_HEIGHT - height) / 2, width, height);
         });
     }
 
