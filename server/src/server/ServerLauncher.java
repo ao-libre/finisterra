114dda66b7aac5ed62387216c8ffe0643e788600
Joaquin <jopiodz00@gmail.com>
2019-07-15 12:51:04 -0300

    Implemented Server.json & I/O

--- server/src/server/ServerLauncher.java
+++ server/src/server/ServerLauncher.java
@@ -1,11 +1,28 @@
 package server;
 
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.esotericsoftware.minlog.Log;
 import server.core.Finisterra;
 
 public class ServerLauncher {
 
     public static void main(String[] arg) {
-        new HeadlessApplication(new Finisterra(7666, 7667));
+
+        /**
+         * Load desktop Server.json or create default.
+         */
+        ServerConfiguration config = ServerConfiguration.loadConfig("Server.json");
+        if (config == null) {
+            Log.info("ServerLauncher", "Server configuration file < Server.json > not found, creating default.");
+            config = ServerConfiguration.createConfig();
+            config.save();
+        }
+
+        /**
+         * Launch application
+         */
+        new HeadlessApplication(new Finisterra(config));
     }
+
+
 }
