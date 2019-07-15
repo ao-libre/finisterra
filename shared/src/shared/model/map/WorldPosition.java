965e041c0dcaa0c1234557d9e94b058128602130
Joaquín <37908806+jopiortiz@users.noreply.github.com>
2019-07-15 14:24:15 -0200

    Server configuration file. `Server.json` (#136)
    * Implemented Server.json & I/O
    
    * Optimized imports, Cleanedup code & Rearranged Entries.
    
    * Added used config properties log dumps.
    
    * Pretty print Server.json

--- shared/src/shared/model/map/WorldPosition.java
+++ shared/src/shared/model/map/WorldPosition.java
@@ -22,6 +22,11 @@
  * @version 0.1
  * @author Rodrigo Troncoso
  * @version 0.1
+ * @author Rodrigo Troncoso
+ * @version 0.1
+ * @since 2014-04-10
+ * <p>
+ * Stores data about a position in the world
  * @since 2014-04-10
  * <p>
  * Stores data about a position in the world
