9f59f37cd6804b302768d64d4db90de0ad0089cd
Derillo <donderillo@gmail.com>
2019-07-18 16:49:29 -0300

    added visual iconGraph based on spells.json for spells slot

--- client/src/game/ui/SpellSlot.java
+++ client/src/game/ui/SpellSlot.java
@@ -119,7 +119,7 @@ public class SpellSlot extends ImageButton {
 
     private Texture getSpellIcon() {
         if (icon == null) {
-            icon = new Texture(Gdx.files.local(Resources.GAME_SPELL_ICONS_PATH + spell.getFxGrh() + ".png"));
+            icon = new Texture(Gdx.files.local(Resources.GAME_SPELL_ICONS_PATH + spell.getIconGrh() + ".png"));
         }
         return icon;
     }
