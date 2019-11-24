111f54fc203a38597d707cebe98e4acb5f57d0b2
Derillo <donderillo@gmail.com>
2019-11-23 23:42:59 -0300

    Corregimos nombre de variable para muscia de fondo

--- client/src/game/screens/GameScreen.java
+++ client/src/game/screens/GameScreen.java
@@ -209,8 +209,8 @@ public class GameScreen extends ScreenAdapter implements WorldScreen {
     public void dispose() {
         world.getSystem(ClientSystem.class).stop();
         world.getSystem(GUI.class).dispose();
-        backGroundMusic.setLooping ( false );
-        backGroundMusic.stop ();
+        backgroundMusic.setLooping ( false );
+        backgroundMusic.stop ();
     }
 
 }
