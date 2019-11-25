816fc9d8129a5bdbe7482b3633a6a822316742f1
Derillo <donderillo@gmail.com>
2019-11-25 19:05:14 -0300

    Revert "Merged duplicate switch branch"
    This reverts commit fd501f37c34d36e04a4b677ea5192b32fbe5d51c.

--- client/src/game/managers/AOInputProcessor.java
+++ client/src/game/managers/AOInputProcessor.java
@@ -161,6 +161,8 @@ public class AOInputProcessor extends Stage {
                 use();
                 break;
             case AOKeys.ATTACK_1:
+                attack();
+                break;
             case AOKeys.ATTACK_2:
                 attack();
                 break;
