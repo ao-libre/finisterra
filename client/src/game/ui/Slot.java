24f28181fc890b64cb32bc12878410b2d81edfa3
bestiardo <bestiardo1@gmail.com>
2019-11-27 00:02:54 -0300

    Items Tooltip primera implementacion
    Muestra  Nombre, Tipo, Valor y Cantidad para todos los objetos.
    *Para Arma el subtipo (kind)
    *Para armas y flechas
          daño max y min
    *Para armaduras, cascos y escudos
          defensa  maxima  y minima.
    *Para las pociones
          el max y min de recuperacion
          el tiempo de efecto
    falta meterle la traducciones si es que es posible

--- client/src/game/ui/Slot.java
+++ client/src/game/ui/Slot.java
@@ -4,12 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.Tooltip;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.esotericsoftware.minlog.Log;
+import entity.character.equipment.Shield;
 import game.handlers.ObjectHandler;
 import game.utils.Skins;
 import game.utils.WorldUtils;
-import shared.objects.types.Obj;
+import shared.objects.types.*;
 
 import java.util.Optional;
 
@@ -25,6 +33,7 @@ public class Slot extends ImageButton {
     private Optional<Item> item = Optional.empty();
 
     private boolean selected;
+    private Tooltip tooltip;
 
     Slot() {
         super(Skins.COMODORE_SKIN, "icon-container");
@@ -49,6 +58,14 @@ public class Slot extends ImageButton {
 
     void setItem(Item item) {
         this.item = Optional.ofNullable(item);
+        if (item == null) {
+            return;
+        }
+        if (tooltip != null) {
+            removeListener(tooltip);
+        }
+        tooltip = getTooltip( item );
+        addListener( tooltip );
     }
 
     @Override
@@ -78,4 +95,67 @@ public class Slot extends ImageButton {
         this.selected = selected;
     }
 
+    public Tooltip< Actor > getTooltip(Item item) {
+        int objID = item.objId;
+        ObjectHandler objectHandler = WorldUtils.getWorld().orElse(null).getSystem(ObjectHandler.class);
+        Optional<Obj> obj = objectHandler.getObject(objID);
+        Actor content  = createTooltipContent ( obj.get() );
+        return new Tooltip<>(content);
+    }
+    private Actor createTooltipContent(Obj obj) {
+        String name = obj.getName();
+        Type objType = obj.getType ();
+
+        Table table = new Table();
+        table.pad(0, 10, 10, 0);
+        table.setSkin( Skins.COMODORE_SKIN );
+        table.background( getBackground() );
+        Label title = new Label(" - " + name + " - ", Skins.COMODORE_SKIN, "title-no-background");
+        Label type = new Label(objType.toString () , Skins.COMODORE_SKIN, "desc-no-background");
+        table.add("").prefWidth( 200 ).fillX().row();
+        table.add(title).center().row();
+        table.add(type).center().row();
+        table.add("").row();
+
+        switch (objType){
+            case WEAPON:
+                WeaponObj weaponObj = (WeaponObj) obj;
+                table.add("kind = " + weaponObj.getKind ().toString ()).left().row();
+                table.add("Max hit = " + weaponObj.getMaxHit ()).left().row();
+                table.add("Min hit = " + weaponObj.getMinHit ()).left().row();
+                break;
+            case POTION:
+                PotionObj potionObj = (PotionObj) obj;
+                table.add("Kind = " +potionObj.getKind ().toString ()).left().row();
+                table.add("Max = " + potionObj.getMax ()).left().row();
+                table.add("Min = " + potionObj.getMin ()).left().row();
+                table.add("EffectTime = " + potionObj.getEffecTime ()).left().row();
+                break;
+            case ARMOR:
+                ArmorObj armorObj = (ArmorObj) obj;
+                table.add("Max Def = " + armorObj.getMaxDef()).left().row();
+                table.add("Min Def = " + armorObj.getMinDef()).left().row();
+                break;
+            case HELMET:
+                HelmetObj helmetObj = (HelmetObj) obj;
+                table.add("Max Def = " + helmetObj.getMaxDef()).left().row();
+                table.add("Min Def = " + helmetObj.getMinDef()).left().row();
+                break;
+            case ARROW:
+                ArrowObj arrowObj = (ArrowObj) obj;
+                table.add("Max Hit = " + arrowObj.getMaxHit()).left().row();
+                table.add("Min Hit = " + arrowObj.getMinHit()).left().row();
+                break;
+            case SHIELD:
+                ShieldObj shieldObj = (ShieldObj) obj;
+                table.add("Max Def = " + shieldObj.getMaxDef()).left().row();
+                table.add("Min Def = " + shieldObj.getMinDef()).left().row();
+
+                break;
+        }
+        table.add("Value = " + obj.getValue()).left().row();
+        table.add("Count = " + item.map(item1 -> item1.count).orElse(1) ).left().row();
+        table.add("").row();
+        return table;
+    }
 }
