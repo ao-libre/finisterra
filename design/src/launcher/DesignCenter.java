71abca160bde4baf34a864cf4cefd2062549c5a1
Joaquín <jopiodz00@gmail.com>
2019-12-08 18:42:35 -0300

    Update DesignCenter.java

--- design/src/launcher/DesignCenter.java
+++ design/src/launcher/DesignCenter.java
@@ -17,6 +17,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.esotericsoftware.minlog.Log;
 import design.screens.ScreenEnum;
 import design.screens.ScreenManager;
 import design.screens.views.View;
