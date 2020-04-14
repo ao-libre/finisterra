b82333a99b3f3ac3eaf84a0d50d58ffd51e09184
bestiardo <bestiardo1@gmail.com>
2020-04-14 16:53:31 -0300

    usamos el Screen manager para asignar la musica segun corresponda

--- client/src/game/screens/ScreenManager.java
+++ client/src/game/screens/ScreenManager.java
@@ -2,10 +2,12 @@ package game.screens;
 
 import com.badlogic.gdx.Screen;
 import game.AOGame;
+import game.systems.resources.MusicSystem;
 import net.mostlyoriginal.api.system.core.PassiveSystem;
 
 public class ScreenManager extends PassiveSystem {
     private AOGame game;
+    private MusicSystem musicSystem;
 
     public ScreenManager(AOGame game) {
         this.game = game;
@@ -13,6 +15,15 @@ public class ScreenManager extends PassiveSystem {
 
     // Show in the game the screen which enum type is received
     public void to(ScreenEnum screen) {
+        switch( screen ){
+            case LOGIN:
+                musicSystem.playMusic( 101 );
+                musicSystem.fadeInMusic( 1f,20f );
+                break;
+            case GAME:
+                musicSystem.playMusic( 1 );
+                break;
+        }
         game.setScreen(screen.get());
     }
 
