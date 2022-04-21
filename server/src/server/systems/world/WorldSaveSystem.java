a775b661215a8a4b8572192e3c0f5d98e5f942b0
cpbeto <41555339+cpbeto@users.noreply.github.com>
2022-04-20 23:53:44 -0300

    Remueve artemis-fluid de WorldSaveSystem

--- server/src/server/systems/world/WorldSaveSystem.java
+++ server/src/server/systems/world/WorldSaveSystem.java
@@ -1,25 +1,25 @@
 package server.systems.world;
 
-import com.artemis.Aspect;
-import com.artemis.E;
-import com.artemis.annotations.Wire;
+import com.artemis.annotations.All;
+import com.artemis.systems.IntervalIteratingSystem;
 import component.entity.character.Character;
 import component.entity.character.info.Name;
-import org.jetbrains.annotations.NotNull;
 import server.systems.account.UserSystem;
 
-@Wire
-public class WorldSaveSystem extends IntervalFluidIteratingSystem {
+@All({Character.class, Name.class}) // Nos suscribimos a solamente a PJs
+public class WorldSaveSystem extends IntervalIteratingSystem {
 
     private UserSystem userSystem;
 
     public WorldSaveSystem(float interval) {
-        super(Aspect.all(Character.class, Name.class), interval);
+        // Acá hay un tema de API de Artemis... Que debería proveer un constructor que tome solo el intervalo
+        // @todo: Abrir un issue upstream
+        super(null, interval);
     }
 
     @Override
-    protected void process(@NotNull E e) {
-        userSystem.save(e.id(), () -> {
-        });
+    protected void process(int entityId) {
+        // @todo: ¿Por qué el guardado está en otro sistema, si este es el sistema de guardado?
+        userSystem.save(entityId, () -> {}); // @todo: Que el runnable sea opcional o algo así
     }
 }
