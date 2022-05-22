54839bb7a2e81161c2380eb20fa4141eb5fff4e5
cpbeto <41555339+cpbeto@users.noreply.github.com>
2022-05-21 21:11:40 -0300

    Remueve artemis-fluid de ItemSystem

--- server/src/server/systems/world/entity/item/ItemSystem.java
+++ server/src/server/systems/world/entity/item/ItemSystem.java
@@ -1,15 +1,15 @@
 package server.systems.world.entity.item;
 
 import com.artemis.Component;
-import com.artemis.E;
-import com.artemis.annotations.Wire;
+import com.artemis.ComponentMapper;
 import com.esotericsoftware.minlog.Log;
 import component.entity.character.attributes.Agility;
 import component.entity.character.attributes.Attribute;
 import component.entity.character.attributes.Strength;
-import component.entity.character.info.Bag;
+import component.entity.character.info.*;
 import component.entity.character.states.Buff;
 import component.entity.character.status.Health;
+import component.entity.character.status.Level;
 import component.entity.character.status.Mana;
 import net.mostlyoriginal.api.system.core.PassiveSystem;
 import server.systems.config.ObjectSystem;
@@ -31,12 +31,9 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
-import static com.artemis.E.E;
-
 /**
  * It keeps logic regarding items, how to use, to know if they are 'usable' or 'equipable'
  */
-@Wire
 public class ItemSystem extends PassiveSystem {
 
     private ItemUsageSystem itemUsageSystem;
@@ -44,6 +41,18 @@ public class ItemSystem extends PassiveSystem {
     private WorldEntitiesSystem worldEntitiesSystem;
     private EntityUpdateSystem entityUpdateSystem;
     private SoundEntitySystem soundEntitySystem;
+
+    ComponentMapper<Name> mName;
+    ComponentMapper<CharHero> mCharHero;
+    ComponentMapper<Health> mHealth;
+    ComponentMapper<Mana> mMana;
+    ComponentMapper<Level> mLevel;
+    ComponentMapper<Gold> mGold;
+    ComponentMapper<Buff> mBuff;
+    ComponentMapper<Agility> mAgility;
+    ComponentMapper<Strength> mStrength;
+    ComponentMapper<Bag> mBag;
+    ComponentMapper<SpellBook> mSpellBook;
 
     public ItemSystem() {
     }
@@ -61,7 +70,7 @@ public class ItemSystem extends PassiveSystem {
         return object.map(ItemUtils::canUse).orElse(false);
     }
 
-    public void use(int player, Bag.Item item) {
+    public void use(int playerId, Bag.Item item) {
         Optional<Obj> object = objectSystem.getObject(item.objId);
         object.ifPresent(obj -> {
             Type objType = obj.getType();
@@ -76,109 +85,106 @@ public class ItemSystem extends PassiveSystem {
 
                     switch (potion.getKind()) {
                         case HP:
-                            Health health = E(player).getHealth();
+                            Health health = mHealth.get(playerId);
                             health.min = Math.min(health.min + random, health.max);
                             components.add(health);
                             break;
                         case MANA:
-                            Mana mana = E(player).getMana();
-                            final int level = E(player).levelLevel();
+                            Mana mana = mMana.get(playerId);
+                            final int level = mLevel.get(playerId).getLevel();
                             mana.min += mana.max * 0.04f + (level >> 1) + 40 / level;
                             mana.min = Math.min(mana.min, mana.max);
                             components.add(mana);
                             break;
                         case AGILITY:
-                            Agility agility = E(player).getAgility();
+                            Agility agility = mAgility.get(playerId);
                             agility.setCurrentValue(agility.getBaseValue() + random);
-                            E(player).buff().getBuff().addAttribute(agility, potion.getEffecTime());
-                            sendAttributeUpdate(player, agility, E(player).getBuff());
+                            mBuff.create(playerId).addAttribute(agility, potion.getEffecTime());
+                            sendAttributeUpdate(playerId, agility, mBuff.get(playerId));
                             break;
                         case POISON:
                         case STRENGTH:
-                            Strength strength = E(player).getStrength();
+                            Strength strength = mStrength.get(playerId);
                             strength.setCurrentValue(strength.getBaseValue() + random);
-                            E(player).buff().getBuff().addAttribute(strength, potion.getEffecTime());
-                            sendAttributeUpdate(player, strength, E(player).getBuff());
+                            mBuff.create(playerId).addAttribute(strength, potion.getEffecTime());
+                            sendAttributeUpdate(playerId, strength, mBuff.get(playerId));
                             break;
                     }
                     // Notify update to user
-                    EntityUpdate update = EntityUpdateBuilder.of(player).withComponents(components.toArray(new Component[0])).build();
+                    EntityUpdate update = EntityUpdateBuilder.of(playerId).withComponents(components.toArray(new Component[0])).build();
                     entityUpdateSystem.add(update, UpdateTo.ENTITY);
-                    soundEntitySystem.add(player, 46);
+                    soundEntitySystem.add(playerId, 46);
                     // TODO remove from inventory
                     break;
                 case SPELL:
                     SpellObj spellObj = (SpellObj) obj;
-                    if (E(player).charHeroHeroId() != 0) {
-                        E(player).spellBookAddSpell(spellObj.getSpellIndex());
+                    if (mCharHero.get(playerId).getHeroId() != 0) {
+                        mSpellBook.get(playerId).addSpell(spellObj.getSpellIndex());
                     }
-                    if (E(player).getSpellBook().getMsj().equals("hechiso agregado")) {
-                        soundEntitySystem.add(player, 109);
-
+                    if (mSpellBook.get(playerId).getMsj().equals("hechiso agregado")) {
+                        soundEntitySystem.add(playerId, 109);
                     }
-                    Log.info(E(player).nameText() + " " + E(player).getSpellBook().getMsj());
+                    Log.info(mName.get(playerId).text + " " + mSpellBook.get(playerId).getMsj());
                     break;
                 case GOLD:
-                    E playerUser = E(player);
-                    int gold = item.count + playerUser.getGold().getCount();
-                    playerUser.goldCount(gold);
-                    removeGold(player);
+                    int goldCount = item.count + mGold.get(playerId).getCount();
+                    mGold.get(playerId).setCount(goldCount);
+                    removeGold(playerId);
                     break;
             }
         });
     }
 
-    protected void sendAttributeUpdate(int player, Attribute attribute, Buff buff) {
-        EntityUpdate updateAGI = EntityUpdateBuilder.of(E(player).id()).withComponents(attribute, buff).build();
+    protected void sendAttributeUpdate(int playerId, Attribute attribute, Buff buff) {
+        EntityUpdate updateAGI = EntityUpdateBuilder.of(playerId).withComponents(attribute, buff).build();
         entityUpdateSystem.add(updateAGI, UpdateTo.ENTITY);
     }
 
-    private void removeGold(int player) {
-        E entity = E(player);
-        Bag.Item[] items = entity.getBag().items;
+    private void removeGold(int playerId) {
+        Bag.Item[] items = mBag.get(playerId).items;
         for (int i = 0; i < items.length; i++) {
             if (items[i] != null) {
                 if (objectSystem.getObject(items[i].objId).get().getType().equals(Type.GOLD)) {
                     items[i].count = 0;
                     //notifica el incremento de oro en el jugador
-                    EntityUpdateBuilder goldUpdate = EntityUpdateBuilder.of(player);
-                    goldUpdate.withComponents(E(player).getGold());
-                    worldEntitiesSystem.sendEntityUpdate(player, goldUpdate.build());
+                    EntityUpdateBuilder goldUpdate = EntityUpdateBuilder.of(playerId);
+                    goldUpdate.withComponents(mGold.get(playerId));
+                    worldEntitiesSystem.sendEntityUpdate(playerId, goldUpdate.build());
                     //notifica la remocion del item y se actualiza el inventario
                     InventoryUpdate update = new InventoryUpdate();
                     update.remove(i);
-                    worldEntitiesSystem.sendEntityUpdate(player, update);
-                    worldEntitiesSystem.sendEntityUpdate(player, E(player).goldCount());
+                    worldEntitiesSystem.sendEntityUpdate(playerId, update);
+                    worldEntitiesSystem.sendEntityUpdate(playerId, mGold.get(playerId));
                 }
             }
         }
     }
 
-    public void equip(int player, int index, Bag.Item item) {
+    public void equip(int playerId, int index, Bag.Item item) {
         InventoryUpdate update = new InventoryUpdate();
         // TODO convert InventoryUpdate into EntityUpdate
-        modifyUserEquip(player, item, index, update);
-        worldEntitiesSystem.sendEntityUpdate(player, update);
+        modifyUserEquip(playerId, item, index, update);
+        worldEntitiesSystem.sendEntityUpdate(playerId, update);
     }
 
-    private void modifyUserEquip(int player, Bag.Item item, int index, InventoryUpdate update) {
+    private void modifyUserEquip(int playerId, Bag.Item item, int index, InventoryUpdate update) {
         Optional<Obj> object = objectSystem.getObject(item.objId);
         object.ifPresent(obj -> {
             item.equipped = !item.equipped;
             update.add(index, item);
             if (item.equipped) {
-                discardItems(E(player), index, obj.getType(), update);
+                discardItems(playerId, index, obj.getType(), update);
             }
-            equipItem(player, obj, item.equipped);
+            equipItem(playerId, obj, item.equipped);
         });
     }
 
-    private void equipItem(int player, Obj item, boolean equipped) {
-        (equipped ? itemUsageSystem.WEAR : itemUsageSystem.TAKE_OFF).accept(player, item);
+    private void equipItem(int playerId, Obj item, boolean equipped) {
+        (equipped ? itemUsageSystem.WEAR : itemUsageSystem.TAKE_OFF).accept(playerId, item);
     }
 
-    private void discardItems(E entity, int index, Type type, InventoryUpdate update) {
-        Bag.Item[] items = entity.getBag().items;
+    private void discardItems(int playerId, int index, Type type, InventoryUpdate update) {
+        Bag.Item[] items = mBag.get(playerId).items;
         for (int i = 0; i < items.length; i++) {
             if (items[i] != null && index != i) {
                 int inventoryIndex = i;
